8740febf75fbc54c906982ca5c2ad61305ceb68f
Pustam Rai <github.pustam@gmail.com>
2025-05-04 17:07:47 +0545

    implement Google authentication routes and middleware setup

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,45 @@
+import passport from "passport";
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+// callback
+userRouter.get(
+  "/google/callback",
+  // if callback failed
+  passport.authenticate("google", { failureRedirect: "/" }),
+
+  // if success redirect back to app
+  (req, res) => {
+    res.redirect(`${process.env.CLIENT_URI}/auth/success`);
+  }
+
+  // testing purpose
+  // (req, res) => {
+  //   res.send(`Login success! Welcome ${req.user.name}`);
+  // }
+);
+
+// logout
+userRouter.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) return res.status(500).send("logout failed");
+    res.redirect(process.env.CLIENT_URI);
+  });
+});
+
+// frontend
+userRouter.get("/me", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json(req.user);
+  } else {
+    res.status(401).json({ message: "Not authenticate" });
+  }
+});
+
+export default userRouter
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,42 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
+import session from 'express-session'
+import passport from './config/password.js'
+import userRouter from './routes/auth.routes.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 8002
+
+// middlewares
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        sameSite: "none",
+        secure: true
+    }
+}))
+
+app.use(express.json())
+app.use(passport.initialize())
+app.use(passport.session())
+
+// end points
+app.use('/api/auth', userRouter)
+
+
+app.get('/', (req, res) => {
+    res.send('welcome to google authentication')
+})
 
 // connect to db
 connectDB()
@@ -19,12 +50,3 @@ connectDB()
     console.log('mongoDB connection failed: ', err.message)
     process.exit(1) // process code 1 means exit with failure, 0 means success
 }) 
-
-// middlewares
-app.use(cors())
-app.use(express.json())
-
-
-app.get('/', (req, res) => {
-    res.send('welcome to google authentication')
-})
