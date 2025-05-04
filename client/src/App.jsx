d6da8d803f44d0ee7eae007b879560b75c193f52
Pustam Rai <github.pustam@gmail.com>
2025-05-04 18:30:32 +0545

    initialize frontend with components and pages

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+
+function App() {
+  
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
